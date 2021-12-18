8a74861a16bf17ea85985874a100c779deb16d16
Jeff <jolson615@gmail.com>
2021-12-18 11:23:32 -0500

    adds api call and squirrel colors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Squirrel from "./Components/Squirrel";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      squirrels: []
+    };
   }
-  
+
   // add a componentDidMount lifecycle method to fetch data from the API
+  componentDidMount() {
+    fetch(
+      "https://data.cityofnewyork.us/resource/vfnx-vebw.json?$$app_token=7JFnCP0dSrA4XEFaxkffxCHCd&running=true"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ squirrels: data });
+      })
+      .catch((e) => console.log(e));
+  }
 
   render() {
     return (
       <div className="App">
         <h1>2018 NYC Squirrel Survey Data</h1>
+        {this.state.squirrels.map((squirrel) => (
+          <Squirrel sdata={squirrel} />
+        ))}
       </div>
     );
   }
--- src/Components/Squirrel.js
+++ src/Components/Squirrel.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Squirrel(props) {
   return (
     <div className="Squirrel">
-      <h3>SQUIRREL CARD!</h3>
+      <h3>
+        {props.sdata.age} {props.sdata.primary_fur_color}
+      </h3>
     </div>
   );
 }
